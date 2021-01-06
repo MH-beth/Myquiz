3c3d658e8e700c010a251b42bdf3a8da918e01d6
MH15O <76598767+MH15O@users.noreply.github.com>
2021-01-06 16:58:25 +0100

    Add files via upload

--- register.js
+++ register.js
@@ -1,15 +1,23 @@
+
+function userRegistered(fname,lname,username,password,gender){
+    localStorage.setItem('fname', fname);
+    localStorage.setItem('lname', lname);
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
+    localStorage.setItem('gender', gender);
+}
+
+
+
 function user(){
-    var Fname=document.userinfo.fname.value;
-    var Lname = document.userinfo.Lname.value;
-    var username = document.userinfo.username.value;
-    var password = document.userinfo.password.value;
-    var gender = document.userinfo.gender.value;
-    console.log(Fname);
-    console.log(Lname);
-    console.log(username);
-    console.log(password);
-    console.log(gender);
-    document.write(Fname ,Lname, username, password, gender  );
-    var userinfo = 'user firstname '+ Fname + 'username '+ username+ 'passworrd' + password + gender
-    document.write(userinfo);
+    let Fname=document.userinfo.fname.value;
+    let Lname = document.userinfo.Lname.value;
+    let username = document.userinfo.username.value;
+    let password = document.userinfo.password.value;
+    let gender = document.userinfo.gender.value;
+    userRegistered(Fname,Lname,username,password,gender);
+    document.getElementById('statue').textContent = `user ${username} s'est inscris avec sucess`;
+    document.getElementById('login').innerText = `
+    username : ${localStorage.getItem('username')}
+    password : ${localStorage.getItem('password')}`
 }
